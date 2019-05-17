bbe22e979b63beb841c7481e9e581a5d76f4d0cd
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-05-17 22:49:49 +0200

    commented out descriptive console.logs triggered by handlers of Algolia Input

--- client/src/components/PlacesInput.js
+++ client/src/components/PlacesInput.js
@@ -15,20 +15,20 @@ const PlacesInput = ({ handleLocation }) => {
 
       onChange={handleLocation}
 
-      onSuggestions={({ rawAnswer, query, suggestions }) =>
-        console.log('Fired when dropdown receives suggestions. You will receive the array of suggestions that are displayed.')}
+      onSuggestions={''/*({ rawAnswer, query, suggestions }) =>
+        console.log('Fired when dropdown receives suggestions. You will receive the array of suggestions that are displayed.')*/}
         
-      onCursorChanged={({ rawAnswer, query, suggestion, suggestonIndex }) =>
-        console.log('Fired when arrows keys are used to navigate suggestions.')}
+      onCursorChanged={''/*({ rawAnswer, query, suggestion, suggestonIndex }) =>
+        console.log('Fired when arrows keys are used to navigate suggestions.')*/}
 
-      onClear={() =>
-        console.log('Fired when the input is cleared.')}
+      onClear={''/*() =>
+        console.log('Fired when the input is cleared.')*/}
 
-      onLimit={({ message }) =>
-        console.log('Fired when you reached your current rate limit.')}
+      onLimit={''/*({ message }) =>
+        console.log('Fired when you reached your current rate limit.')*/}
 
-      onError={({ message }) =>
-        console.log('Fired when we could not make the request to Algolia Places servers for any reason but reaching your rate limit.')}
+      onError={''/*({ message }) =>
+        console.log('Fired when we could not make the request to Algolia Places servers for any reason but reaching your rate limit.')*/}
     />
   )
 }
