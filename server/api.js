86a6370ea411cdcc379da4de3cdffaaedd248ff6
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-06-05 13:30:24 +0200

    added unit query to darksky default fetch request

--- server/api.js
+++ server/api.js
@@ -4,7 +4,8 @@ require('dotenv').config()
 const dark = axios.create({
   baseURL: `https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}`,
   params: {
-    exclude: 'currently,flags'
+    exclude: 'currently,flags',
+    units: 'si'
   }
 })
 
