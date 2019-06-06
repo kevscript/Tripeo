b5d888d73c2bf646ecd8453f166e92afbd9c1d4f
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-06-06 09:24:01 +0200

    implemented open/close behaviour for WeatherCards

--- client/src/components/DailyCard.js
+++ client/src/components/DailyCard.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
+import Arrow from '../assets/icons/arrow.svg'
 
-const DailyItem = styled.li`
+const DailyItem = styled.div`
+  cursor: pointer;
+  position: relative;
   display: flex;
+  min-height: 114px;
   flex-direction: row;
+  align-items: center;
   width: 100%;
   background: ${props => props.theme.colors.primary};
   padding: 30px;
@@ -13,10 +18,12 @@ const DailyItem = styled.li`
 const InfoContainer = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
 `
 
 const DescContainer = styled.div`
-  padding-left: 30px;
+  padding: 0 50px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -33,10 +40,32 @@ const Desc = styled.p`
   font-weight: 600;
 `
 
-const DailyCard = ({ forecast, cp }) => {
+const ArrowContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  right: 30px;
+  width: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Img = styled.img`
+  display: block;
+  width: 100%;
+  height: auto;
+  transition: transform 0.3s ease-in-out;
+
+  ${({ open }) => open && `
+    transform: rotate(180deg);
+  `}
+`
+
+const DailyCard = ({ forecast, cp, handleClick, open }) => {
   return (
     <div>
-      <DailyItem key={cp.timestamp}>
+      <DailyItem key={cp.timestamp} onClick={handleClick}>
         <InfoContainer>
           <Where>{cp.location.name}</Where>
           <When>{new Date(cp.timestamp).toLocaleDateString()}</When>
@@ -44,6 +73,9 @@ const DailyCard = ({ forecast, cp }) => {
         <DescContainer>
           <Desc>{forecast.daily.data[0].summary}</Desc>
         </DescContainer>
+        <ArrowContainer>
+          <Img src={Arrow} open={open} />
+        </ArrowContainer>
       </DailyItem>
     </div>
   )
--- client/src/components/WeatherCard.js
+++ client/src/components/WeatherCard.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import DailyCard from './DailyCard'
 import HourlyCard from './HourlyCard'
 
 const CardContainer = styled.div`
-  margin: 0;
+  margin: 5px 0;
 `
 
 const WeatherCard = ({ cp, forecast }) => {
+
+  const [open, setOpen] = useState(false)
+
+  const handleOpen = () => {
+    setOpen(!open)
+  }
+
   return (
     <CardContainer>
-      <DailyCard cp={cp} forecast={forecast} />
-      {forecast.daily.data.length > 0 && forecast.hourly.data.map(hour => {
+      <DailyCard cp={cp} forecast={forecast} handleClick={handleOpen} open={open} />
+      {open && forecast.daily.data.length > 0 && forecast.hourly.data.map(hour => {
         return (
           <HourlyCard key={hour.time} data={hour} timeZone={forecast.timezone}/>
         )
