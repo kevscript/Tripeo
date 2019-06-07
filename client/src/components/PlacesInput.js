0ac9130980e1f860424483c0ccb3e64ee23af2ff
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-06-07 13:47:31 +0200

    restyled algolia places input component

--- client/src/components/PlacesInput.js
+++ client/src/components/PlacesInput.js
@@ -1,36 +1,44 @@
 import React from 'react'
 import AlgoliaPlaces from 'algolia-places-react'
 import '../styles/algoliaplaces.css'
+import styled from 'styled-components'
 
+const Label = styled.span`
+  font-size: 14px;
+  margin-bottom: 5px;
+`
 
 const PlacesInput = ({ handleLocation }) => {
   return (
-    <AlgoliaPlaces
-      placeholder='Location (city, address)'
+    <>
+      <Label>Location (cities, adresses, ...)</Label>
+      <AlgoliaPlaces
+        placeholder='eg. Paris, Ile-de-France, France'
 
-      options={{
-        appId: process.env.REACT_APP_ALGOLIA_APP_ID,
-        apiKey: process.env.REACT_APP_ALGOLIA_API_KEY,
-        // Other options from https://community.algolia.com/places/documentation.html#options
-      }}
+        options={{
+          appId: process.env.REACT_APP_ALGOLIA_APP_ID,
+          apiKey: process.env.REACT_APP_ALGOLIA_API_KEY,
+          // Other options from https://community.algolia.com/places/documentation.html#options
+        }}
 
-      onChange={handleLocation}
+        onChange={handleLocation}
 
-      onSuggestions={''/*({ rawAnswer, query, suggestions }) =>
-        console.log('Fired when dropdown receives suggestions. You will receive the array of suggestions that are displayed.')*/}
-        
-      onCursorChanged={''/*({ rawAnswer, query, suggestion, suggestonIndex }) =>
-        console.log('Fired when arrows keys are used to navigate suggestions.')*/}
+        onSuggestions={''/*({ rawAnswer, query, suggestions }) =>
+          console.log('Fired when dropdown receives suggestions. You will receive the array of suggestions that are displayed.')*/}
 
-      onClear={''/*() =>
-        console.log('Fired when the input is cleared.')*/}
+        onCursorChanged={''/*({ rawAnswer, query, suggestion, suggestonIndex }) =>
+          console.log('Fired when arrows keys are used to navigate suggestions.')*/}
 
-      onLimit={''/*({ message }) =>
-        console.log('Fired when you reached your current rate limit.')*/}
+        onClear={''/*() =>
+          console.log('Fired when the input is cleared.')*/}
 
-      onError={''/*({ message }) =>
-        console.log('Fired when we could not make the request to Algolia Places servers for any reason but reaching your rate limit.')*/}
-    />
+        onLimit={''/*({ message }) =>
+          console.log('Fired when you reached your current rate limit.')*/}
+
+        onError={''/*({ message }) =>
+          console.log('Fired when we could not make the request to Algolia Places servers for any reason but reaching your rate limit.')*/}
+      />
+    </>
   )
 }
 
