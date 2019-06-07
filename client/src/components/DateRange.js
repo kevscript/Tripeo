4725c3f2c4fccbc08ab46051319ff7e9ebb79076
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-06-07 13:48:09 +0200

    restyled daterange component

--- client/src/components/DateRange.js
+++ client/src/components/DateRange.js
@@ -4,10 +4,11 @@ import "react-datepicker/dist/react-datepicker.css"
 import styled from 'styled-components'
 
 const RangeContainer = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  justify-content: space-between;
+  align-items: space-between;
 `
 
 const StyledDateInput = styled(DatePicker)`
@@ -15,7 +16,20 @@ const StyledDateInput = styled(DatePicker)`
   outline: 0;
   border: 1px solid lightgrey;
   border-radius: 3px;
-  font-size: 15px;
+  font-size: 14px;
+  width: 125px;
+  color: ${props => props.theme.colors.primary};
+`
+
+const Label = styled.span`
+  font-size: 14px;
+  margin-bottom: 5px;
+`
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   margin: 10px 0;
 `
 
@@ -29,26 +43,32 @@ const DateRange = ({
 }) => {
   return (
     <RangeContainer>
-      <StyledDateInput
-        placeholderText='From'
-        selected={startDate}
-        selectsStart
-        startDate={startDate}
-        endDate={endDate}
-        onChange={handleStartDateChange}
-        dateFormat="dd / MM / yyyy"
-        minDate={startMin}
-      />
-      <StyledDateInput
-        placeholderText='To'
-        selected={endDate}
-        selectsEnd
-        startDate={startDate}
-        endDate={endDate}
-        onChange={handleEndDateChange}
-        dateFormat="dd / MM / yyyy"
-        minDate={endMin}
-      />
+      <InputContainer>
+        <Label>From (included)</Label>
+        <StyledDateInput
+          placeholderText='01 / 01 / 2020'
+          selected={startDate}
+          selectsStart
+          startDate={startDate}
+          endDate={endDate}
+          onChange={handleStartDateChange}
+          dateFormat="dd / MM / yyyy"
+          minDate={startMin}
+        />
+      </InputContainer>
+      <InputContainer>
+        <Label>To (included)</Label>
+        <StyledDateInput
+          placeholderText='02 / 01 / 2020'
+          selected={endDate}
+          selectsEnd
+          startDate={startDate}
+          endDate={endDate}
+          onChange={handleEndDateChange}
+          dateFormat="dd / MM / yyyy"
+          minDate={endMin}
+        />
+      </InputContainer>
     </RangeContainer>
   )
 }
