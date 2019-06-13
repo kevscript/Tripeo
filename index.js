47246d602c09407cd609cc9d9b1c605e459e34c5
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-06-13 13:22:17 +0200

    updated build for heroku

--- index.js
+++ index.js
@@ -26,12 +26,12 @@ app.get('/api/:lat/:lng/:time', (req, res) => {
 const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'app/build')));
+  // Serve static files from the React frontend app
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
-  // Handle React routing, return all requests to React app
+  // Anything that doesn't match the above, send back index.html
   app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'app/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
