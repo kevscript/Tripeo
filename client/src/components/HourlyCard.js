1cb07b8c79cd0ed1996b157bb9938daa43a6747d
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-06-24 18:23:36 +0200

    fixed slider

--- client/src/components/HourlyCard.js
+++ client/src/components/HourlyCard.js
@@ -116,16 +116,10 @@ const HourlyCard = ({ data, timeZone }) => {
   const sliderSettings = {
     arrow: true,
     speed: 800,
-    slidesToShow: 5,
+    slidesToShow: 4,
     slidesToScroll: 1,
+    swipeToSlide: true,
     responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        }
-      },
       {
         breakpoint: 800,
         settings: {
